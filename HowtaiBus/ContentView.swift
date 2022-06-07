f67cd693b8805166c3293e8d1e93a919d1dd1cb8
Yu <kiras0518@gmail.com>
2022-06-07 14:03:52 +0800

    Initial Commit

---
+++ HowtaiBus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HowtaiBus
+//
+//  Created by yukun on 2022/6/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HowtaiBus/HowtaiBusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowtaiBusApp.swift
+//  HowtaiBus
+//
+//  Created by yukun on 2022/6/7.
+//
+
+import SwiftUI
+
+@main
+struct HowtaiBusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
